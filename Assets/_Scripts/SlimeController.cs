f5818e1cf9eae4e712b68bb98b71056139bf767d
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2019-06-10 09:39:18 -0300

    Several attempts to get the slime to move with touch controls. nothing yet.

---
+++ Assets/_Scripts/SlimeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class SlimeController : MonoBehaviour
+{
+
+    private Vector3 touchPosition;
+    private Rigidbody2D rb;
+    private Vector3 direction;
+    private float moveSpeed = 10f;
+
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0.0f;
+            direction = (touchPosition - transform.position);
+            rb.velocity = new Vector2(direction.x, direction.y) * moveSpeed;
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
+    }
+}
