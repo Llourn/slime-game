90991b4d6df3f6d2a018c82fa8a01a5ebdd54c48
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2019-06-17 22:04:14 -0300

    Lots of progress! Landed on a nice mesh deformation solution for the slime! Moving on to slime trail, eating and growing mechanics!

---
+++ Assets/DeformExterior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeformExterior : MonoBehaviour
+{
+    public float moveForce = 10f;
+    private void OnTriggerStay(Collider other)
+    {
+        MeshDeformer deformer = other.gameObject.GetComponent<MeshDeformer>();
+        if (deformer)
+        {
+            Debug.Log("Is this working??");
+            deformer.AddDeformingForce(other.ClosestPoint(transform.position), moveForce);
+        }
+    }
+}
---
+++ Assets/_Scripts/CatchUp.cs
@@ -0,0 +1,50 @@
+﻿using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class CatchUp : MonoBehaviour
+{
+    public Rigidbody innerSlimeRB;
+    public float moveSpeed = 1.0f;
+
+    public Transform innerSlime;
+    public Transform outerSlime;
+    public float distanceThreshold;
+    public float minCatchUpSpeed = 1.0f;
+
+    
+    public Vector3 moveStretch;
+    public float stretchingThreshold;
+    public float stretchSpeed = 1.0f;
+
+
+
+    private void Update()
+    {
+
+        Vector3 velocity = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0.0f, CrossPlatformInputManager.GetAxis("Vertical")) * moveSpeed;
+        innerSlimeRB.AddForce(velocity, ForceMode.Force);
+
+
+
+        float slimeDistance = Vector3.Distance(innerSlime.position, outerSlime.position);
+        if (slimeDistance > distanceThreshold)
+        {
+            float catchUpSpeed = minCatchUpSpeed * ((slimeDistance - distanceThreshold) / 0.1f);
+            catchUpSpeed = (catchUpSpeed > minCatchUpSpeed) ? catchUpSpeed : minCatchUpSpeed;
+            outerSlime.position = Vector3.Lerp(outerSlime.position, innerSlime.position, Time.deltaTime * catchUpSpeed);
+        }
+
+        //if (Vector3.Distance(innerSlime.position, outerSlime.position) > stretchingThreshold)
+        //{
+        //    outerSlime.localScale = Vector3.Lerp(outerSlime.localScale, moveStretch, Time.deltaTime * stretchSpeed);
+        //}
+        //else
+        //{
+        //    outerSlime.localScale = Vector3.Lerp(outerSlime.localScale, Vector3.one, Time.deltaTime * stretchSpeed);
+        //}
+
+        //outerSlime.LookAt(innerSlime);
+    }
+
+    
+}
