4d68599268db61e94a8129d37af5a36d7b0abf68
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2019-06-23 09:43:22 -0300

    Added destructible walls.

---
+++ Assets/WallTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTrigger : MonoBehaviour
+{
+    public GameObject unbroken;
+    public GameObject broken;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Slime"))
+        {
+            unbroken.SetActive(false);
+            broken.SetActive(true);
+        }
+    }
+}
