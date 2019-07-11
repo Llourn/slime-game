98a78d25b072a743218567cd53c084fe40cc05a6
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2019-07-11 13:03:15 -0300

    Changed the slime's material. transparent materials allow particle effects to render in front of them. Also began camera follow script.

---
+++ Assets/_Scripts/Collect.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Collect : MonoBehaviour
+{
+    public float absorbSpeed = 1.0f;
+    public float disintegrateSpeed = 1.0f;
+    public Vector3 smallestSize;
+    public Transform collectionContainer;
+
+    private float moveTowardsStep;
+    private float disintegrateStep;
+
+    private void Update()
+    {
+        moveTowardsStep = absorbSpeed * Time.deltaTime;
+        disintegrateStep = disintegrateSpeed * Time.deltaTime;
+        AbsorbTheChildren();
+    }
+
+    private void AbsorbTheChildren()
+    {
+        foreach (Transform child in collectionContainer)
+        {
+            if (Vector3.Distance(collectionContainer.position, child.position) > 0.3)
+                child.position = Vector3.MoveTowards(child.position, collectionContainer.position, moveTowardsStep);
+            else
+                child.localScale = Vector3.Lerp(child.localScale, smallestSize, disintegrateStep);
+            if (child.localScale.x <= 0.04f) Destroy(child.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Human"))
+        {
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            other.gameObject.GetComponent<NavMeshAgent>().enabled = false;
+            other.transform.SetParent(collectionContainer);
+            other.transform.rotation = Random.rotation;
+        }
+
+        if (other.gameObject.layer == 13)
+        {
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            other.transform.SetParent(collectionContainer);
+        }
+    }
+
+
+}
