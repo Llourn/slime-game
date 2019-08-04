96dde0d481a11411077bf7c0906ca07051312572
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2019-08-03 22:03:31 -0300

    Updated the LWRP to allow for custom rendering. Updated rendering for humans to allow for tracking behind objects.

--- Assets/_Scripts/SlimeController.cs
+++ Assets/_Scripts/SlimeController.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Vehicles.Ball;
-using Vector3 = UnityEngine.Vector3;
 
 [RequireComponent(typeof(Rigidbody))]
 public class SlimeController : MonoBehaviour
@@ -10,7 +9,6 @@ public class SlimeController : MonoBehaviour
     private bool _moveEnabled;
 
     // Reference cache
-    Rigidbody _rbody;
     public Ball ball;
 
     public LayerMask touchMask;
@@ -18,60 +16,15 @@ public class SlimeController : MonoBehaviour
 
     private void Start()
     {
-        _rbody = GetComponent<Rigidbody>();
         ball = GetComponent<Ball>();
     }
 
     private void Update()
     {
-
         Vector3 velocity = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0.0f, CrossPlatformInputManager.GetAxis("Vertical")) * moveSpeed;
         ball.Move(velocity, false);
-
-        //if (Input.touchCount > 0)
-        //{
-        //    Touch touch = Input.GetTouch(0);
-
-        //    switch(touch.phase)
-        //    {
-        //        case TouchPhase.Began:
-        //            if(HitSlime(touch))
-        //            {
-        //                _moveEnabled = true;
-        //                _rbody.useGravity = false;
-        //            }
-        //            // Check if the slime was hit with the ray.
-        //            // If the slime was hit, enable move mode where swiping on screen moves the slime.
-        //            break;
-        //        case TouchPhase.Moved:
-        //            // if move mode enabled, move the slime.
-        //            if(_moveEnabled)
-        //            {
-        //                Vector3 pos = _rbody.position;
-        //                Vector2 dPos = touch.deltaPosition;
-        //                _rbody.MovePosition(new Vector3(pos.x + dPos.x, pos.y, pos.z + dPos.y) * moveSpeed);
-        //            }
-        //            Debug.Log("Delta Position: " + touch.deltaPosition);
-        //            break;
-        //        case TouchPhase.Ended:
-        //            // if move mode enabled, release the slime. 
-        //            _moveEnabled = false;
-        //            _rbody.useGravity = true;
-        //            break;
-        //        default:
-        //            break;
-        //    }
-
-        //}
-
     }
 
-    private void LateUpdate()
-    {
-        
-
-        
-    }
 
     private bool HitSlime(Touch touch)
     {
---
+++ Assets/_Scripts/SlimeDetector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeDetector : MonoBehaviour
+{
+    [SerializeField] private FindSafety findSafety = null;
+    [SerializeField] private LayerMask slimeMask = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (findSafety.IsSafe()) return;
+        // if (other.CompareTag("Slime")) IsSlimeInLineOfSight();
+        if(other.CompareTag("Slime")) findSafety.SelectTarget();
+    }
+
+    private bool IsSlimeInLineOfSight()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, GameManager.instance.player.transform.position - transform.position, out hit, Mathf.Infinity, slimeMask))
+        {
+            Debug.DrawRay(transform.position, (GameManager.instance.player.transform.position - transform.position) * hit.distance, Color.red);
+            if (hit.transform.CompareTag("Slime") || hit.transform.CompareTag("Eater"))
+            {
+                findSafety.SelectTarget();
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+}
