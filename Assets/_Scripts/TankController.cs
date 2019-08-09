b363488c01f29a2e40eaa016a859bb7c11f8ca59
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2019-08-09 11:46:01 -0300

    Pushing changes before importing NavmeshComponents package from Unity's github repo. https://github.com/Unity-Technologies/NavMeshComponents

---
+++ Assets/_Scripts/TankController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TankController : MonoBehaviour
+{
+    [SerializeField] private float maxFiringRange = 10.0f;
+
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        SetTarget(GameManager.instance.player.transform.position);
+    }
+
+    private void SetTarget(Vector3 targetPosition)
+    {
+        _agent.destination = targetPosition;
+    }
+    
+}
